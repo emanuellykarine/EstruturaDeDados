d9fd09126d89fbf47b52f24aeef9ae4fc5671da1
Emanuelly Karine <emanuellyfernandes77@gmail.com>
2025-05-19 14:36:58 +0000

    Vetor

---
+++ Vetor/Pratica1/ColocacaoErradaExcecao.java
@@ -0,0 +1,7 @@
+package Vetor.Pratica1;
+
+public class ColocacaoErradaExcecao extends RuntimeException{
+    public ColocacaoErradaExcecao (String err) {
+        super(err);
+    } 
+}
---
+++ Vetor/Pratica1/Vetor.java
@@ -0,0 +1,10 @@
+package Vetor.Pratica1;
+
+public interface Vetor {
+    public Object elemAtRank(int r) throws ColocacaoErradaExcecao; //Retorna o elementro na colocação r
+    public void insertAtRank(int r, Object o); //Insere um novo elemento na colocação r
+    public Object replaceAtRank(int r, Object o) throws ColocacaoErradaExcecao; // Substitui o elemento na colocação r por o e retorna o antigo elemento
+    public Object removeAtRank(int r) throws ColocacaoErradaExcecao; //Remove e retorna o elemento na colocação r
+    public int size();
+    public boolean isEmpty();
+}
