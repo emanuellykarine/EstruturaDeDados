7a7ee8a30de8e3c7237273c21d8ddfb8a5ee29fe
Emanuelly Karine <emanuellyfernandes77@gmail.com>
2025-09-25 17:19:42 +0000

    rotações

--- AVL/AVL/AVLTeste.java
+++ AVL/AVL/AVLTeste.java
@@ -2,37 +2,41 @@ package AVL;
 
 public class AVLTeste{
     public static void main(String[] args){
-        // AVLClasse avl = new AVLClasse(10);
 
-        // avl.addChild(5);
-        // avl.addChild(15);
-        // avl.addChild(2);
-        // avl.addChild(8);
-        // avl.addChild(22);
+        System.out.println("AVL 1 Rotação simples a esquerda");
+        AVLClasse avl = new AVLClasse(10);
+        avl.addChild(5);
+        avl.addChild(15);
+        avl.addChild(2);
+        avl.addChild(8);
+        avl.addChild(22);
 
-        // avl.printArvore();
-        // System.out.println();
+        avl.printArvore();
+        System.out.println();
 
-        // avl.addChild(25);
-        // avl.printArvore();
-        // System.out.println();
+        avl.addChild(25);
+        avl.printArvore();
+        System.out.println();
 
-        // avl.remove(5);
-        // avl.printArvore();
-        // System.out.println();
+        avl.remove(5);
+        avl.printArvore();
+        System.out.println();
 
-        // AVLClasse avl2 = new AVLClasse(50);
-        // avl2.addChild(20);
-        // avl2.addChild(90);
-        // avl2.addChild(10);
-        // avl2.addChild(40);
+        System.out.println("AVL 2 Rotação dupla a direita");
+        AVLClasse avl2 = new AVLClasse(50);
+        avl2.addChild(20);
+        avl2.addChild(90);
+        avl2.addChild(10);
+        avl2.addChild(40);
 
-        // avl2.printArvore();
-        // System.out.println();
-        // avl2.addChild(30);
+        avl2.printArvore();
+        System.out.println();
+        avl2.addChild(30);
     
-        // avl2.printArvore();
+        avl2.printArvore();
+        System.out.println();
 
+        System.out.println("AVL 3 Rotação dupla a esquerda");
         AVLClasse avl3 = new AVLClasse(50);
         avl3.addChild(20);
         avl3.addChild(80);
