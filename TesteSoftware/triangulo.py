5787421c5afdaed670a887d1b9494e6d25d4fe4a
Emanuelly Karine <emanuellyfernandes77@gmail.com>
2026-03-17 13:22:53 +0000

    Teste de software triangulo

--- TesteSoftware/triangulo.py
+++ TesteSoftware/triangulo.py
@@ -1,6 +1,6 @@
 a, b, c = map(int, input().split(" "))
  
-if (a + b < c) or (a + c < b) or (b + c < a):
+if (a + b <= c) or (a + c <= b) or (b + c <= a):
     print('Nao é um triangulo')
 elif (a == b) and (a == c) :
     print('Equilatero')
@@ -9,3 +9,15 @@ elif (a==b) or (a==c) or (b==c):
 else:
     print('Escaleno')
 
+# @emanuellykarine ➜ /workspaces/EstruturaDeDados/TesteSoftware (main) $ python triangulo.py
+# 1 2 3
+# Escaleno
+# @emanuellykarine ➜ /workspaces/EstruturaDeDados/TesteSoftware (main) $ python triangulo.py
+# 5 6 4
+# Escaleno
+# @emanuellykarine ➜ /workspaces/EstruturaDeDados/TesteSoftware (main) $ python triangulo.py
+# 4 5 4 
+# Isósceles
+# @emanuellykarine ➜ /workspaces/EstruturaDeDados/TesteSoftware (main) $ python triangulo.py
+# 4 4 4
+# Equilatero
